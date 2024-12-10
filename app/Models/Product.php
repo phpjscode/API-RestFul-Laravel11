fa4a65b71c3e084001a4a63b5be83979d8917028
Edwin <edwinjuarez24x@gmail.com>
2024-12-10 01:24:18 +0000

    Implementando las propiedades para Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const PRODUCTO_DISPONIBLE = 'disponible';
+    const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function estaDisponible()
+    {
+        return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
 }
