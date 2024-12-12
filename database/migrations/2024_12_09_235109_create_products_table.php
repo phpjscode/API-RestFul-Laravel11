220e736ee8944d8d740dc1907e46ff2d82bf226a
Edwin <edwinjuarez24x@gmail.com>
2024-12-12 00:39:01 +0000

    Implementando la migración para categories

--- database/migrations/2024_12_09_235109_create_products_table.php
+++ database/migrations/2024_12_09_235109_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->string('description', 1000);
             $table->integer('quantity')->unsigned();
             $table->string('status')->default(Product::PRODUCTO_NO_DISPONIBLE);
             $table->string('image');
--- database/migrations/2024_12_09_235200_create_categories_table.php
+++ database/migrations/2024_12_09_235200_create_categories_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description', 1000);
             $table->timestamps();
+
+            
         });
     }
 
