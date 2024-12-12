827bbd3aab5c07e891ae13eca86fae015212a449
Edwin <edwinjuarez24x@gmail.com>
2024-12-12 21:22:14 +0000

    Creando el Factory para Category

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * php artisan make:factory CategoryFactory --model=Category
+     */
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word,
+            'description' => fake()->paragraph(1),
+        ];
+    }
+}
