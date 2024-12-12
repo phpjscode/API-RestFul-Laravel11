7dfc9dd2d4d57f8066a9d38533a52a2ea36275d5
Edwin <edwinjuarez24x@gmail.com>
2024-12-12 23:30:00 +0000

    Usando los Factories desde el DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,37 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        // 
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate();
+
+        // $cantidadUsuarios = 200;
+        $cantidadUsuarios = 1000;
+        $cantidadCategorias = 30;
+        $cantidadProductos = 1000;
+        $cantidadTransacciones = 1000;
+
+        // factory(User::class, $cantidadUsuarios)->create(); //Laravel < 8
+        User::factory($cantidadUsuarios)->create();
+        Category::factory($cantidadCategorias)->create();
+        
+        Product::factory($cantidadProductos)
+            ->create()
+            ->each(function ($producto) {
+                $categorias = Category::all()->random(mt_rand(1, 5))->pluck('id');
+
+                $producto->categories()->attach($categorias);
+            });
+
+        Transaction::factory($cantidadTransacciones)->create();
     }
 }
