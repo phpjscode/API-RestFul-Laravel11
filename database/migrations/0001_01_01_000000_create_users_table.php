1a499a59a0f6ae0264f053a4e8e3a6492d956d79
Edwin <edwinjuarez24x@gmail.com>
2024-12-12 00:15:40 +0000

    Implementando la migración para users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -17,6 +18,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('verified')->default(User::USUARIO_NO_VERIFICADO);
+            $table->string('varification_token')->nullable();
+            $table->string('admin')->default(User::USUARIO_REGULAR);
             $table->rememberToken();
             $table->timestamps();
         });
