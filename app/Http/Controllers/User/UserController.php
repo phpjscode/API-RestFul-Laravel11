60dec9616b6e90903de181b1cd6e2b1ec9fb4797
Edwin <edwinjuarez24x@gmail.com>
2024-12-15 01:08:53 +0000

    Implementando el método store de UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -32,7 +33,44 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $reglas = [
+        //     'name' => 'required',
+        //     'email' => 'required|email|unique:users',
+        //     'password' => 'required|min:6|confirmed',
+        // ];
+
+        $reglas = [
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:6', 'confirmed'],
+        ];
+
+        // $this->validate($request, $reglas); // En Laravel 10<
+        // request()->validate($reglas);
+        $request->validate($reglas);
+
+        $campos = $request->all();
+        // dd($campos);
+        // $campos['password'] = bcrypt($request->password);
+        $campos['password'] = Hash::make($request->password);
+        $campos['verified'] = User::USUARIO_NO_VERIFICADO;
+        $campos['verification_token'] = User::generarVerificationToken();
+        $campos['admin'] = User::USUARIO_REGULAR;
+
+        $usuario = User::create($campos);
+
+        // // 2da Forma
+        // $usuario = User::create([
+        //     'name' => $request->name,
+        //     'email' => $request->email,
+        //     'password' => bcrypt($request->password),
+        //     'password_confirmation' => $request->password,
+        //     'verified' => User::USUARIO_NO_VERIFICADO,
+        //     'verification_token' => User::generarVerificationToken(),
+        //     'admin' => User::USUARIO_REGULAR,
+        // ]);
+
+        return response()->json(['data' => $usuario], 201);
     }
 
     /**
