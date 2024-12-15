a65f097af0024b886dc214c22844c6262f103d2e
Edwin <edwinjuarez24x@gmail.com>
2024-12-15 17:40:41 +0000

    Implementando el método index de SellerController

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Seller;
 
-use App\Http\Controllers\Controller;
+use App\Models\Seller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SellerController extends Controller
 {
@@ -12,37 +13,15 @@ class SellerController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $vendedores = Seller::has('products')->get();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        return response()->json(['data' => $vendedores], 200);
     }
 
     /**
      * Display the specified resource.
      */
     public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
     {
         //
     }
