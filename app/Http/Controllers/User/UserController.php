f2b38b810425bd4863be3cb268fa189fe251bf2c
Edwin <edwinjuarez24x@gmail.com>
2024-12-17 03:54:59 +0000

    Usando los métodos generalizados para respuestas de error

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -141,13 +141,16 @@ class UserController extends ApiController
 
         if ($request->has('admin')) {
             if (!$user->esVerificado()) {
-                return response()->json(['error' => 'Unicamente los usuarios verificados pueden cambiar su valor de administrador.', 'code' => 409], 409);
+                // return response()->json(['error' => 'Unicamente los usuarios verificados pueden cambiar su valor de administrador.', 'code' => 409], 409);
+
+                return $this->errorResponse('Unicamente los usuarios verificados pueden cambiar su valor de administrador.', 409);
             }
             $user->admin = $request->admin;
         }
 
         if (!$user->isDirty()) { // isDirty determina si alguno de los atributos del modelo ha cambiado respecto al valor actual
-            return response()->json(['error' => 'Se debe especificar al menos un valor diferente para actualizar.', 'code' => 422], 422);
+            // return response()->json(['error' => 'Se debe especificar al menos un valor diferente para actualizar.', 'code' => 422], 422);
+            return $this->errorResponse('Se debe especificar al menos un valor diferente para actualizar.', 422);
         }
 
         $user->save();
